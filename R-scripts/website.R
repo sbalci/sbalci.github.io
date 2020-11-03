bf1baa9d8b494157357c0d67025ba7c39cb4d1d0
SERDAR BALCI <drserdarbalci@gmail.com>
2020-11-03 14:49:02 +0300

    update

---
+++ R-scripts/website.R
@@ -0,0 +1,9 @@
+distill::import_post(
+    url = "https://sbalci.github.io/pubmed/BibliographicStudies.html",
+    slug = "bibliometricstudies",
+    # date = ,
+    date_prefix = NULL,
+    check_license = FALSE,
+    overwrite = TRUE,
+    view = TRUE
+)
