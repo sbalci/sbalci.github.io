8c2388c437a272bf4c5827549ea6acaf66500a6b
sbalci <drserdarbalci@gmail.com>
2023-05-19 14:50:35 +0300

    update

--- R-scripts/website.R
+++ R-scripts/website.R
@@ -1,9 +1,22 @@
-distill::import_post(
-    url = "https://raw.githubusercontent.com/sbalci/bibliometrics/master/BibliographicStudies.Rmd",
-    slug = "bibliometricstudies",
-    # date = ,
-    date_prefix = NULL,
-    check_license = FALSE,
-    overwrite = TRUE,
-    view = TRUE
-)
+# Patolojide Bilişim
+
+updated_text <- readLines(con = "https://raw.githubusercontent.com/sbalci/patoloji-ve-bilisim/main/README.md")
+ 
+writeLines(text = updated_text, con = "./_posts/patolojide-bilisim/updated_text.md")
+
+header_text <- readLines(con = "./_posts/patolojide-bilisim/_header_patolojide-bilisim.Rmd")
+
+post_text <- c(paste0(header_text), paste0(updated_text))
+
+writeLines(text = post_text, con = "./_posts/patolojide-bilisim/patolojide-bilisim.Rmd")
+
+
+# distill::import_post(
+#     url = "https://raw.githubusercontent.com/sbalci/bibliometrics/master/BibliographicStudies.Rmd",
+#     slug = "bibliometricstudies",
+#     # date = ,
+#     date_prefix = NULL,
+#     check_license = FALSE,
+#     overwrite = TRUE,
+#     view = TRUE
+# )
